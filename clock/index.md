d499fbd19a4d62f30bcb93a51ebd68405e01956c
Kalvir Sandhu <kalvir.sandhu@gmail.com>
2025-02-02 15:39:42 +0000

    ship a new clock for all

---
+++ clock/index.md
@@ -0,0 +1,19 @@
+---
+layout: layouts/home.njk
+title: Clock
+templateClass: tmpl-post
+eleventyNavigation:
+  key: Clock
+  order: 5
+---
+
+A different clock to help you meditate and concentrate on your work.
+
+Think of this is an alternative clock you have on your desk that you can use while you're doing your work.
+
+The concept is that the man created a sun dial to better plan their day, understand at what time they'd do a specific task. But in today's culture this leads to a number of stresses and work that you have to do outside of the task or work you're doing.
+
+This ticks in what I call a 1, 5, 60. It still uses the concept of seconds but the time indicators are unique to your mind when it's running. So it becomes your personal clock. 
+
+<div id="clock" class="clock"></div>
+
--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ function getRandomInt(min, max) {
 class Clock {
 	constructor(clock) {
 		this.clock = clock;
-    this.currentPst = document.getElementById("current-pst-time");
+    //this.currentPst = document.getElementById("current-pst-time");
     this.currentInterval = 1;
     this.lastMarker = " - o";
     setInterval(this.startCurrentPst.bind(this), 1000);
@@ -98,7 +98,7 @@ class Clock {
   }
 
   startCurrentPst() {
-    this.currentPst.innerHTML = new Date().toLocaleString('en', {timeZone: 'America/Vancouver'});
+    //this.currentPst.innerHTML = new Date().toLocaleString('en', {timeZone: 'America/Vancouver'});
   }
 
   start() {
@@ -108,7 +108,7 @@ class Clock {
     // work out next mars interval
     this.lastMarker = this.currentInterval;
     this.currentInterval = getRandomInt(1,60);
-    console.log('um - ' + this.currentInterval);
+    console.log('o - ' + this.currentInterval);
 
     // fire next sun mars ping
     setTimeout(this.start.bind(this), this.currentInterval * 1000);
